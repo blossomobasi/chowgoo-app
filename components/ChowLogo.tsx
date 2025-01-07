0ed04745c629026f9eceec40f515bf441fc9ac2e
Blossom Obasi <blossomobasi2@gmail.com>
2025-01-07 15:31:56 +0100

    Add ChowLogo component for displaying the logo image

---
+++ components/ChowLogo.tsx
@@ -0,0 +1,12 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+
+const ChowLogo = () => {
+	return (
+		// <View>
+		<Image source={require("../assets/images/chow-logo.jpeg")} height={10} width={10} />
+		// </View>
+	);
+};
+
+export default ChowLogo;
