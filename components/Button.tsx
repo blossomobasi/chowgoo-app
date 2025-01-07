02bea9279acd152149176d49c57df91e64f72151
Blossom Obasi <blossomobasi2@gmail.com>
2025-01-07 15:31:49 +0100

    Add Button component for reusable UI functionality

---
+++ components/Button.tsx
@@ -0,0 +1,54 @@
+import { Text, StyleSheet, useColorScheme, Pressable } from "react-native";
+import React from "react";
+import { Link } from "expo-router";
+import { Colors } from "@/constants/Colors";
+
+interface ButtonProps {
+	title: string;
+	type?: "primary" | "secondary";
+	link?: string;
+	customStyles?: object;
+	onPress?: () => void;
+}
+
+const Button = ({ title, type = "primary", link, customStyles, onPress }: ButtonProps) => {
+	const colorScheme = useColorScheme();
+	const theme = Colors[colorScheme ?? "light"];
+
+	if (link) {
+		return (
+			<Link
+				href={link as any}
+				style={[styles.button, { backgroundColor: type === "primary" ? theme.primary : "white" }, customStyles]}
+			>
+				<Text style={[styles.text, type === "secondary" && { color: theme.primary }]}>{title}</Text>
+			</Link>
+		);
+	}
+
+	return (
+		<Pressable
+			style={[styles.button, { backgroundColor: type === "primary" ? theme.primary : "white" }, customStyles]}
+			onPress={onPress}
+		>
+			<Text style={[styles.text, type === "secondary" && { color: theme.primary }]}>{title}</Text>
+		</Pressable>
+	);
+};
+
+export default Button;
+
+const styles = StyleSheet.create({
+	button: {
+		width: "100%",
+		padding: 15,
+		borderRadius: 10,
+		alignItems: "center",
+	},
+	text: {
+		color: "white",
+		fontWeight: "500",
+		fontSize: 16,
+		textAlign: "center",
+	},
+});
