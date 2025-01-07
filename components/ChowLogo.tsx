cbb8098a581cbb11dfb413da81d6741348f9afd5
Blossom Obasi <blossomobasi2@gmail.com>
2025-01-07 19:05:04 +0100

    Enhance ChowLogo component with link functionality and improved styling

--- components/ChowLogo.tsx
+++ components/ChowLogo.tsx
@@ -1,12 +1,25 @@
-import { View, Text, Image } from "react-native";
+import { Image, StyleSheet } from "react-native";
 import React from "react";
+import { Link, LinkProps } from "expo-router";
 
-const ChowLogo = () => {
+interface Props {
+	href?: LinkProps["href"];
+}
+
+const ChowLogo = ({ href = "/(tabs)" }: Props) => {
 	return (
-		// <View>
-		<Image source={require("../assets/images/chow-logo.jpeg")} height={10} width={10} />
-		// </View>
+		<Link href={href}>
+			<Image source={require("../assets/images/chow-logo.jpeg")} style={styles.logo} />
+		</Link>
 	);
 };
 
 export default ChowLogo;
+
+const styles = StyleSheet.create({
+	logo: {
+		width: 150,
+		height: 150,
+		objectFit: "contain",
+	},
+});
