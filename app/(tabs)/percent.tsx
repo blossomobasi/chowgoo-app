e55ee479ad5da98f3796ef5a0eb0b9b2972c6ef2
Blossom Obasi <blossomobasi2@gmail.com>
2025-01-08 12:54:33 +0100

    Add PercentScreen component for displaying percentage information

---
+++ app/(tabs)/percent.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const PercentScreen = () => {
+	return (
+		<View>
+			<Text>PercentScreen</Text>
+		</View>
+	);
+};
+
+export default PercentScreen;
